c8a14b2f4d40d3b88b23f33576e99341cd0a3496
Joël Gabriel Robles Gasser <joel-robles-gasser@hotmail.ch>
2020-04-10 01:21:15 +0200

    test

--- MTGCardsOnline/backend/models/models/model-card.js
+++ MTGCardsOnline/backend/models/models/model-card.js
@@ -2,15 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CardSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-  },
-  price: Number,
-  set: {
-    type: String,
-    trim: true,
-  },
+  name: { type: String, trim: true },
+  price: { type: Number, trim: true },
+  set: { type: String, trim: true },
 });
 
 const Card = mongoose.model("Card", CardSchema);
