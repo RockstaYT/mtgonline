5e0fe6cd3bf849a76850e60e50a5a738b2d0332d
Joël Gabriel Robles Gasser <joel-robles-gasser@hotmail.ch>
2020-04-10 01:19:01 +0200

    test

--- MTGCardsOnline/backend/config/index.js
+++ MTGCardsOnline/backend/config/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 module.exports = {
-  MONGO_URI: process.env.MONGO_URI || "mongodb://localhost/test",
-  SERVER_PORT: process.env.SERVER_PORT || 3000,
+  MONGO_URI: process.env.MONGO_URI,
+  SERVER_PORT: process.env.SERVER_PORT,
   APP_TOKEN: process.env.APP_TOKEN,
   APP_SECRET: process.env.APP_SECRET,
   ACCESS_TOKEN: process.env.ACCESS_TOKEN,
