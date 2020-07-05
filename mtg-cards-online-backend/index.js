ba6d74280b2ae7165c5059e142d0db4fdb24f0fc
RoockstaYT <joel-robles-gasser@hotmail.ch>
2020-07-06 01:29:47 +0200

    added git

--- mtg-cards-online-backend/index.js
+++ mtg-cards-online-backend/index.js
@@ -1,6 +1,35 @@
+/*--------------------------Requires--------------------------*/
+var bodyParser = require("body-parser");
+var cors = require("cors");
 var express = require("Express");
-var app = express();
-
+var mongoose = require("mongoose");
 var router = require("./routes.js");
 
+/*--------------------------Inits--------------------------*/
+var app = express();
+
+/*--------------------------Imports--------------------------*/
+var { MONGO_URI, SERVER_PORT } = require("./config");
+
+/*--------------------------Usage--------------------------*/
 app.use("router", router);
+app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cors());
+
+/*--------------------------Database--------------------------*/
+mongoose.connect(
+  MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("conntected to mongoDB");
+  }
+);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
+/*--------------------------Server Port--------------------------*/
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server listening at port: ${SERVER_PORT}`);
+});
