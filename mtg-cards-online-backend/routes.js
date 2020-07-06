602b810ee20ffc905372776a980b5183e7cca887
RoockstaYT <joel-robles-gasser@hotmail.ch>
2020-07-07 00:05:01 +0200

    Added all needed routes

--- mtg-cards-online-backend/routes.js
+++ mtg-cards-online-backend/routes.js
@@ -48,5 +48,32 @@ router.post("set/get_cards", async (req, res) => {
   }
 });
 
+/*Get specific card from specific set.*/
+router.post("set/get_card", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log("ERROR:", error);
+    res.status(404).send(error);
+  }
+});
+
+/*Get all prices from specific card*/
+router.post("card/get_prices", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log("ERROR:", error);
+    res.status(404).send(error);
+  }
+});
+
+/*Get all reprints from specific card*/
+router.post("card/get_reprint", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log("ERROR:", error);
+    res.status(404).send(error);
+  }
+});
+
 /*--------------------------Exports--------------------------*/
 module.exports = router;
