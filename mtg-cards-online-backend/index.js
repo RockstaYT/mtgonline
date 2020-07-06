0b1c40e097d13fd3f1ed3747e90215aada82392c
RoockstaYT <joel-robles-gasser@hotmail.ch>
2020-07-07 00:01:05 +0200

    Added Routes

--- mtg-cards-online-backend/index.js
+++ mtg-cards-online-backend/index.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var cors = require("cors");
 var express = require("Express");
 var mongoose = require("mongoose");
-var router = require("./routes.js");
+var router = require("./routes");
 
 /*--------------------------Inits--------------------------*/
 var app = express();
@@ -12,7 +12,7 @@ var app = express();
 var { MONGO_URI, SERVER_PORT } = require("./config");
 
 /*--------------------------Usage--------------------------*/
-app.use("router", router);
+app.use("/", router);
 app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
@@ -29,6 +29,12 @@ mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 
 /*--------------------------Server Port--------------------------*/
+
+/*app.post("/", async (req, res) => {
+  try {
+    console.log("Done.");
+  } catch (error) {}
+});*/
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening at port: ${SERVER_PORT}`);
--- mtg-cards-online-backend/routes.js
+++ mtg-cards-online-backend/routes.js
@@ -8,8 +8,44 @@ var router = express.Router();
 
 /*--------------------------POST--------------------------*/
 
-router.post("", async (req, res) => {
-  console.log("Done.");
+/*Create all sets. If set exists, skip it.*/
+router.post("sets/create_all", async (req, res) => {
+  try {
+    console.log("Creating all sets.");
+    //create all sets
+  } catch (error) {
+    console.log("ERROR:", error);
+    res.status(404).send(error);
+  }
+});
+
+/*Create all cards from specific set. If card alredy exists, skip it.*/
+router.post("set/create_all_cards", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log("ERROR:", error);
+    res.status(404).send(error);
+  }
+});
+
+/*Get all sets.*/
+router.post("sets/get_all", async (req, res) => {
+  try {
+    console.log("Creating all sets.");
+    //create all sets
+  } catch (error) {
+    console.log("ERROR:", error);
+    res.status(404).send(error);
+  }
+});
+
+/*Get all cards from specific set.*/
+router.post("set/get_cards", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log("ERROR:", error);
+    res.status(404).send(error);
+  }
 });
 
 /*--------------------------Exports--------------------------*/
